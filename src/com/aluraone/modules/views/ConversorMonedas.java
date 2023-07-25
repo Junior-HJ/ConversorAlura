58e09199ab77f52a69c0042e7cd0f56bbb04e6aa
Junior-HJ <112132647+Junior-HJ@users.noreply.github.com>
2023-07-25 16:08:13 -0500

    first commit

---
+++ src/com/aluraone/modules/views/ConversorMonedas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.aluraone.modules.views;
+
+/**
+ *
+ * @author Jr
+ */
+public class ConversorMonedas {
+    public static void main(String[] args) {
+        MenuView menuView = new MenuView();
+	menuView.setVisible(true);
+    }
+}
